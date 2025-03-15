63835814d7b4415ef14677c5c06998c7236f58ba
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-15 10:05:52 -0300

    Adicionando os arquivos terraform ao repo

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "regionDefault" {
+  default = "us-east-1"
+}
+
+variable "projectName" {
+  default = "EKS-FOOD-ORDER"
+}
+
+variable "labRole" {
+  default = "arn:aws:iam::239569854352:role/LabRole"
+}
+
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
+}
+
+variable "nodeGroup" {
+  default = "food-order-node-group"
+}
+
+variable "instanceType" {
+  default = "t3.medium"
+}
+
+variable "principalArn" {
+  default = "arn:aws:iam::239569854352:role/voclabs"
+}
+
+variable "policyArn" {
+  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
