dd790e413828fe3c41cb398f559d10c8a64f46b7
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-18 21:03:32 -0300

    Alterando cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "eks-cluster" {
   name     = var.projectName
   role_arn = var.labRole
-
+  
   vpc_config {
     subnet_ids         = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
     security_group_ids = [aws_security_group.sg.id]
