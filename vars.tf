7d3ae2986af9124c87db783689920fbc580f2de3
Diego G. lima <diego.lima@alice.com>
2025-03-21 21:10:32 -0300

    upadte arn

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "projectName" {
 }
 
 variable "labRole" {
-  default = "arn:aws:iam::340103052246:role/LabRole"
+  default = "arn:aws:iam::198212171636:role/LabRole"
 }
 
 variable "accessConfig" {
@@ -23,7 +23,7 @@ variable "instanceType" {
 }
 
 variable "principalArn" {
-  default = "arn:aws:iam::340103052246:role/voclabs"
+  default = "arn:aws:iam::198212171636:role/voclabs"
 }
 
 variable "policyArn" {
