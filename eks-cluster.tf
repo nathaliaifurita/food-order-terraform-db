178643a10de61410fd1c6706e9fcc49e6415f3f1
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-23 10:13:22 -0300

    Alterando Cluster

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,9 +3,11 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = var.labRole
 
   vpc_config {
-    subnet_ids = [
-      aws_subnet.private_subnets[0].id,
-      aws_subnet.private_subnets[1].id
-    ]
+    subnet_ids         = aws_subnet.private_subnets[*].id
+    security_group_ids = [aws_security_group.sg.id]
+  }
+
+  access_config {
+    authentication_mode = var.accessConfig
   }
 }
