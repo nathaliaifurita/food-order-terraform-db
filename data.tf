338bbe7dc043cf5339f712829bbdb60ec27b28ba
Diego G. lima <diego.lima@alice.com>
2025-03-23 16:20:54 -0300

    handle existent subnet or vpc

--- data.tf
+++ data.tf
@@ -1,5 +1,6 @@
+// Busca VPC existente
 data "aws_vpc" "existing_vpc" {
-  count = var.create_vpc ? 0 : 1  // Só busca se não formos criar uma nova
+  count = var.create_vpc ? 0 : 1
 
   filter {
     name   = "cidr-block"
@@ -12,8 +13,9 @@ data "aws_vpc" "existing_vpc" {
   }
 }
 
+// Cria VPC se necessário
 resource "aws_vpc" "main_vpc" {
-  count = var.create_vpc ? 1 : 0  // Só cria se a variável for true
+  count = var.create_vpc ? 1 : 0
 
   cidr_block = "172.31.0.0/16"
 
@@ -27,28 +29,86 @@ locals {
   vpc_id = var.create_vpc ? aws_vpc.main_vpc[0].id : data.aws_vpc.existing_vpc[0].id
 }
 
-data "aws_subnets" "subnets" {
+// Busca subnets públicas existentes
+data "aws_subnet" "existing_public_subnets" {
+  count = var.create_vpc ? 0 : 2
+
   filter {
     name   = "vpc-id"
     values = [local.vpc_id]
   }
+
+  filter {
+    name   = "tag:Environment"
+    values = ["public"]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [element(["us-east-1a", "us-east-1b"], count.index)]
+  }
 }
 
+// Cria subnets públicas se necessário
+resource "aws_subnet" "public_subnets" {
+  count                   = var.create_vpc ? 2 : 0
+  vpc_id                  = local.vpc_id
+  cidr_block              = cidrsubnet("172.31.0.0/16", 4, count.index + 2)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "Public Subnet ${count.index + 1}"
+    Environment = "public"
+    "kubernetes.io/cluster/${var.projectName}" = "shared"
+    "kubernetes.io/role/elb"                   = "1"
+  }
+}
+
+// Busca subnets privadas existentes
+data "aws_subnet" "existing_private_subnets" {
+  count = var.create_vpc ? 0 : 2
+
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
+
+  filter {
+    name   = "tag:Environment"
+    values = ["private"]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = [element(["us-east-1a", "us-east-1b"], count.index)]
+  }
+}
+
+// Cria subnets privadas se necessário
 resource "aws_subnet" "private_subnets" {
-  count             = 2
+  count             = var.create_vpc ? 2 : 0
   vpc_id            = local.vpc_id
-  cidr_block        = var.create_vpc ? cidrsubnet(aws_vpc.main_vpc[0].cidr_block, 4, count.index) : cidrsubnet("172.31.0.0/16", 4, count.index)
+  cidr_block        = cidrsubnet("172.31.0.0/16", 4, count.index)
   availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
 
   tags = {
     Name        = "Private Subnet ${count.index + 1}"
     Environment = "private"
+    "kubernetes.io/cluster/${var.projectName}" = "shared"
+    "kubernetes.io/role/internal-elb"         = "1"
   }
 }
 
+locals {
+  private_subnet_ids = var.create_vpc ? aws_subnet.private_subnets[*].id : data.aws_subnet.existing_private_subnets[*].id
+  public_subnet_ids  = var.create_vpc ? aws_subnet.public_subnets[*].id : data.aws_subnet.existing_public_subnets[*].id
+}
+
+// Grupo de subnets para o RDS
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = aws_subnet.private_subnets[*].id
+  subnet_ids = local.private_subnet_ids
 
   tags = {
     Name        = "RDS Subnet Group"
