10764d44a753ac99ff3fc50c6253f468523fd620
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-22 21:44:06 -0300

    Alterando cluster

--- eks-access-entry.tf
+++ eks-access-entry.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_access_entry" "eks-access-entry" {
-  cluster_name      = aws_eks_cluster.eks-cluster.name
+  cluster_name      = aws_eks_cluster.eks_cluster.name
   principal_arn     = var.principalArn
   kubernetes_groups = ["food-order-api-groups"]
   type              = "STANDARD"
