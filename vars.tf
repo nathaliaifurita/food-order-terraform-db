851b00b4983e434b1f4e303bea56314030fcd322
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-24 18:43:33 -0300

    Alterando de novo

--- vars.tf
+++ vars.tf
@@ -61,15 +61,3 @@ variable "db_password" {
   default     = "postgres"
   sensitive   = true
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
