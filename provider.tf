8b2d11107af86618d4cc60ab4a04edf7ad52f92a
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-24 18:35:28 -0300

    Passando variaveis terraform

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-  assume_role {
-    role_arn = var.principalArn
-  }
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
--- vars.tf
+++ vars.tf
@@ -61,3 +61,15 @@ variable "db_password" {
   default     = "postgres"
   sensitive   = true
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
