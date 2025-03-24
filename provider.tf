88ec1f09d036f93630fb8c71559c40910f94b6d2
nathaliaifurita <nathaliaofreire@gmail.com>
2025-03-24 18:57:28 -0300

    Debug

--- provider.tf
+++ provider.tf
@@ -1,21 +1,7 @@
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 variable "aws_region" {
   default = "us-east-1"
-}
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
 }
