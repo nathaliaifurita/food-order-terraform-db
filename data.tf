581e44b843c9d9324d18ca3fa0dd18d566709914
nathaliaifurita <nathaliaofreire@gmail.com>
2025-05-18 11:01:21 -0300

    Atualizando

--- data.tf
+++ data.tf
@@ -99,35 +99,6 @@ resource "aws_subnet" "private_subnets" {
 //}
 
 
-resource "aws_subnet" "public_subnets" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = cidrsubnet("172.31.0.0/16", 4, count.index + 2)
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "Public Subnet ${count.index + 1}"
-    Environment = "public"
-//    "kubernetes.io/cluster/${var.projectName}" = "shared"
-//    "kubernetes.io/role/elb"                   = "1"
-  }
-}
-
-resource "aws_subnet" "private_subnets" {
-  count             = 2
-  vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = cidrsubnet("172.31.0.0/16", 4, count.index)
-  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
-
-  tags = {
-    Name        = "Private Subnet ${count.index + 1}"
-    Environment = "private"
-//    "kubernetes.io/cluster/${var.projectName}" = "shared"
-//    "kubernetes.io/role/internal-elb"         = "1"
-  }
-}
-
 //Criando subnet se nao tiver
 locals {
   private_subnet_ids = aws_subnet.private_subnets[*].id
